98804c9d5b0ced2e447377346a0487dbd42112e7
Glitch (glitch-hello-website) <none>
2024-09-04 20:24:14 +0000

    🐯✈️ Checkpoint ./script.js:411873/4207

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 import { Voronoi } from "./voronoi.js";
 
 const randomInt = () => {
-    const ranges = [
-      { min: 500, max: 700 },
-      { min: 1000, max: 5000 },
-      { min: 5000, max: 7000 },
-    ];
-    const { min, max } = ranges[Math.floor(Math.random() * ranges.length)];
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+  const ranges = [
+    { min: 500, max: 700 },
+    { min: 1000, max: 5000 },
+    { min: 5000, max: 7000 },
+  ];
+  const { min, max } = ranges[Math.floor(Math.random() * ranges.length)];
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 const numPoints = randomInt();
 
 const points = [];
 
-function drawVoronoi(canvasId,nPoints) {
+function drawVoronoi(canvasId, nPoints) {
   const canvas = document.getElementById(canvasId);
   const ctx = canvas.getContext("2d");
   const canvasWidth = canvas.clientWidth;
   const canvasHeight = canvas.clientHeight;
-  
+
   // Set canvas size
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
-  
+
   for (let i = 0; i < nPoints; i++) {
     points.push({
       x: Math.random() * canvasWidth,
       y: Math.random() * canvasHeight,
     });
   }
-  
+
   const voronoi = new Voronoi();
   const bbox = { xl: 0, xr: canvasWidth, yt: 0, yb: canvasHeight };
   const diagram = voronoi.compute(points, bbox);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // Draw Voronoi cells
   diagram.cells.forEach((cell) => {
@@ -56,8 +58,12 @@ function drawVoronoi(canvasId,nPoints) {
     ctx.stroke();
   });
 }
-
-window.addEventListener("resize", drawVoronoi("background",numPoints));
+let i = 0;
+window.addEventListener("resize", ()=>{
+  i = i +1;
+  console.log(i);
+  drawVoronoi("background",numPoints)
+});
 
 // Initialize on page load
-window.addEventListener("load", drawVoronoi("background",numPoints));
+window.addEventListener("load", drawVoronoi("background", numPoints));
